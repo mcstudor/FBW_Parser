9930827a903bcc51b4043183bdb53cd4f5ffae45
Valdyn Hunt <valdyn@mac.com>
2019-05-28 17:57:37 -0700

    Behavioral - CommandDoSpeedBrake

--- src/sbw/project/cli/parser/cmdstategy/CommandDoSpeedBrake.java
+++ src/sbw/project/cli/parser/cmdstategy/CommandDoSpeedBrake.java
@@ -1,6 +1,10 @@
 package sbw.project.cli.parser.cmdstategy;
 
+import sbw.architecture.datatype.Identifier;
+import sbw.project.cli.action.ActionBehavioral;
 import sbw.project.cli.action.ActionSet;
+import sbw.project.cli.action.command.behavioral.CommandDoDeploySpeedBrake;
+import sbw.project.cli.action.command.behavioral.CommandDoHalt;
 
 import java.text.ParseException;
 
@@ -12,6 +16,19 @@ public class CommandDoSpeedBrake extends CommandChoice {
 
 	@Override
 	public void runCommand() throws ParseException {
+		// DO <id> SPEED BRAKE ON|OFF
 		System.out.println(this.command);
+
+		String[] arr = this.command.split(" ");
+		ActionBehavioral ab = this.actionSet.getActionBehavioral();
+		Identifier id = Validate.makeIdentifier(arr[1]);
+
+		boolean isDeployed = false;
+		if (arr[4].equals("ON")) {
+			isDeployed = true;
+		}
+
+		CommandDoDeploySpeedBrake cd = new CommandDoDeploySpeedBrake(id, isDeployed);
+		ab.submitCommand(cd);
 	}
 }
