3966457e18420dcb4ea34f1753d5c85057b3bc9c
Valdyn Hunt <valdyn@mac.com>
2019-06-04 15:21:47 -0700

    add Part 2 - Structural tests

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,8 +9,9 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args ) throws FileNotFoundException {
-		File file = new File("src/test/cmds.txt");
+		//File file = new File("src/test/cmds.txt");
 		//File file = new File("src/test/csTests.txt");
+		File file = new File("src/test/testing.txt");
 
 		CommandLineInterface cli = new CommandLineInterface();
 
